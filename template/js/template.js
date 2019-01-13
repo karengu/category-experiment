f433cacf7b43968a8a872e3d760500f87b7af6cb
Karen Gu <karengu@Karens-MacBook-Pro.local>
2019-01-13 17:46:34 -0500

    format and wording changes

--- template/js/template.js
+++ template/js/template.js
@@ -531,10 +531,10 @@ function make_slides(f) {
         var testSequenceIndex = 0;
 
 	if (stim.utteranceType == "barePlural") {
-          exp.utterance = stim.objectNamePlural+' '+stim.property+'.';
+          exp.utterance = stim.objectNamePlural+' '+stim.property+'!';
         }
         else if (stim.utteranceType == "specific") {
-          exp.utterance = 'This '+stim.objectNameSingular.toLowerCase()+' '+stim.propertySpecific+'.';
+          exp.utterance = 'This '+stim.objectNameSingular.toLowerCase()+' '+stim.propertySpecific+'!';
 	}
 
         var onClick = function() {
@@ -575,10 +575,8 @@ function make_slides(f) {
 	    }
 	    else {
 	      if (paper.customAttributes.itemsTested == 1) { // first item
-	        setTimeout(function() { // delay showing button to leave for 2 seconds
-                  $('#ddbutton').show();
-                  $('#ddbutton').text('Switch to ringing');
-                }, 2000);
+                $('#ddbutton').show();
+                $('#ddbutton').text('Switch to ringing');
 	      }
 	      exp.squeak.pause();
 	      exp.squeak.currentTime = 0;
@@ -613,11 +611,9 @@ function make_slides(f) {
 	var displayNote = function() {
           paper.customAttributes.demoArrow = paper.path("M100,100 v40").attr({'arrow-end': 'classic-narrow-long', "stroke-width": 4});
           paper.customAttributes.noteDisplay = paper.set();
-	  paper.customAttributes.noteDisplay.push(paper.rect(120, 40, 570, 140).attr({fill: "gray", "fill-opacity": 0, "stroke-width": 0}));
-	  paper.customAttributes.noteDisplay.push(paper.text(400, 70, exp.utterance).attr({"font-size": 18, "fill": "white", "font-weight": "bold"}));
-          paper.customAttributes.noteDisplay.push(paper.text(400, 110, "Test this one first. To test an object,").attr({"font-size": 14, "fill": "white"}));
-          paper.customAttributes.noteDisplay.push(paper.text(400, 130, "click on it to select it and then click on the button.").attr({"font-size": 14, "fill": "white"}));
-          paper.customAttributes.noteDisplay.push(paper.text(400, 160, "Click anywhere inside this box to close.").attr({"font-size": 14, "fill": "white"}));
+	  paper.customAttributes.noteDisplay.push(paper.rect(120, 40, 570, 140).attr({fill: "#ffff99", "fill-opacity": 0, "stroke-width": 0}));
+	  paper.customAttributes.noteDisplay.push(paper.text(400, 100, exp.utterance).attr({"font-size": 20, "fill": "black", "font-weight": "bold"}));
+          paper.customAttributes.noteDisplay.push(paper.text(400, 160, "Click anywhere inside this box to close.").attr({"font-size": 14, "fill": "black"}));
 	  paper.customAttributes.noteDisplay.forEach(function(elem) {
             elem.animate({"fill-opacity": 1, "stroke-opacity": 1}, 1000, "easeInOut");
             elem.click(function(elem) {paper.customAttributes.noteDisplay.remove()});
@@ -650,11 +646,11 @@ function make_slides(f) {
 	paper.customAttributes.firstItemId = 5;
 	paper.customAttributes.testedFirstItem = false;
 	// task information
-	$('#infoParagraph').html('<p>These are the blickets that your colleague left for you to test. First take a look at the note she left you and test the blicket the note is on. (Click on the yellow note to read it.)</p><p>After you are done testing the first '+stim.objectNameSingular.toLowerCase()+', test as many '+stim.objectNamePlural.toLowerCase()+' for squeaking as you would like. When you are done testing for squeaking and want to move on to testing for ringing, hit the <b>Switch to ringing</b> button at the bottom of the screen. You must test at least one other blicket for squeaking.</p>');
+    $('#infoParagraph').html("<p>These are the 20 blickets that your colleague left for you to test. First take a look at the note she left you. (Click on the yellow note to read it.)</p><p>Test the blicket with the note first. To test: click on a blicket, then click <b>Test squeaking</b>.</p><p>Test as many blickets for squeaking as you would like. Any blickets you don't test can be tested for ringing. When you want to switch, click the <b>Swith to ringing</b> cutton at the bottom of the screen, and you will be able to rest for ringing. (You will not be able to test for squeaking again.)");
 	paper.customAttributes.note = paper.set();
-	paper.customAttributes.note.push(paper.rect(100,110,80,80).attr({"fill": "yellow"}));
-	paper.customAttributes.note.push(paper.rect(110,130, 60, 10).attr({"fill": "gray", "stroke-width": 0}));
-	paper.customAttributes.note.push(paper.rect(110, 150, 60, 10).attr({"fill": "gray", "stroke-width": 0}));
+	paper.customAttributes.note.push(paper.rect(100,110,80,80).attr({"fill": "#ffff99"}));
+	paper.customAttributes.note.push(paper.rect(110,130, 60, 8).attr({"fill": "gray", "stroke-width": 0}));
+	paper.customAttributes.note.push(paper.rect(110, 150, 60, 8).attr({"fill": "gray", "stroke-width": 0}));
 	paper.customAttributes.note.forEach(function(elem) {elem.click(displayNote)});
       }
       else if (stim.type == 'end') {
